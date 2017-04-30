ecf7539d04a2e07e05eae70af7c8ec5885e03d7f
Shivendra Kumar Mittal <shivendrakumar2304@gmail.com>
2017-04-30 22:19:24 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/home.css"/>
+<title>College management System</title>
+</head>
+
+<body>
+	<div class="panel panel-default">
+  		<div class="panel-heading"><h1>College Management System</h1></div>
+                <div class="panel-body"><br><br>
+        <div>
+            <img src="images/rtupic.jpg" width="855" style="text-align: center;" height="308" onmousedown="return false;"  />
+        </div><br><br>
+        <div class="middle_text">
+            <p><strong>Indian Institute of Information Technology is a deemed university located in Allahabad,India.</strong></p>
+        </div>
+                
+        <div class="text_para_home">
+            <p> The mission of IIIT, Allahabad is to be a unique and world class nucleating "Apex Center of Excellence" in the area of Information
+			Technology and Allied Sciences, so as to enhance India's Technological strength in IT and for become a pace-setting institution for other similar
+			institutes to be established in the future. IIIT seeks to derive its strength from a linkage with sound Indian traditions of centuries past, and
+			sets out to create knowledge-based resources in regional languages.</p>         
+        </div>   
+                </div>
+        </div>
+    
+    
+</body>
+</html>
